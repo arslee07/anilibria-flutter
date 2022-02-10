e387a93d8846de531278ab2538622c2c0501bc8a
arslee <me@arslee.dev>
2022-02-10 11:15:50 +0900

    fix: Home widget is not rebuilding when the route changes

--- lib/utils/route_maps.dart
+++ lib/utils/route_maps.dart
@@ -2,51 +2,54 @@ import 'package:anilibria_app/features/home/home_page.dart';
 import 'package:anilibria_app/features/player/player_page.dart';
 import 'package:anilibria_app/features/title_info/title_info_page.dart';
 import 'package:anilibria_app/features/titles_search/titles_search_screen.dart';
+import 'package:anilibria_app/utils/keys.dart';
 import 'package:anilibria_app/utils/player_title_info.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final goRouterProvider = Provider(
-  (ref) => GoRouter(routes: [
-    GoRoute(
-      path: '/',
-      redirect: (_) => '/titles',
-    ),
-    GoRoute(
-      path: '/youtube',
-      pageBuilder: (_, __) => const MaterialPage(child: HomePage()),
-    ),
-    GoRoute(
-      path: '/titles',
-      pageBuilder: (_, __) => const MaterialPage(child: HomePage()),
-      routes: [
-        GoRoute(
-          path: 'search',
-          pageBuilder: (_, __) => CustomTransitionPage(
-            child: const TitlesSearchPage(),
-            transitionsBuilder: (_, animation, __, child) =>
-                FadeTransition(opacity: animation, child: child),
-            transitionDuration: const Duration(milliseconds: 150),
-          ),
-        ),
-        GoRoute(
-          path: r':id(\d+)',
-          pageBuilder: (_, state) => MaterialPage(
-            child: TitleInfoPage(int.parse(state.params['id']!)),
-          ),
-          routes: [
-            GoRoute(
-              path: 'player',
-              redirect: (s) =>
-                  s.extra == null ? '/titles/${s.params["id"]!}' : null,
-              pageBuilder: (_, state) => MaterialPage(
-                child: PlayerPage(state.extra! as PlayerTitleInfo),
-              ),
+  (ref) => GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        redirect: (_) => '/titles',
+      ),
+      GoRoute(
+        path: '/youtube',
+        pageBuilder: (_, __) => MaterialPage(child: HomePage(key: homeKey)),
+      ),
+      GoRoute(
+        path: '/titles',
+        pageBuilder: (_, __) => MaterialPage(child: HomePage(key: homeKey)),
+        routes: [
+          GoRoute(
+            path: 'search',
+            pageBuilder: (_, __) => CustomTransitionPage(
+              child: const TitlesSearchPage(),
+              transitionsBuilder: (_, animation, __, child) =>
+                  FadeTransition(opacity: animation, child: child),
+              transitionDuration: const Duration(milliseconds: 150),
             ),
-          ],
-        ),
-      ],
-    ),
-  ]),
+          ),
+          GoRoute(
+            path: r':id(\d+)',
+            pageBuilder: (_, state) => MaterialPage(
+              child: TitleInfoPage(int.parse(state.params['id']!)),
+            ),
+            routes: [
+              GoRoute(
+                path: 'player',
+                redirect: (s) =>
+                    s.extra == null ? '/titles/${s.params["id"]!}' : null,
+                pageBuilder: (_, state) => MaterialPage(
+                  child: PlayerPage(state.extra! as PlayerTitleInfo),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  ),
 );
