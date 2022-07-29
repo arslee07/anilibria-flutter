f73edca519368166b0c09ffa94805c1e90479462
arslee <mail@arslee.me>
2022-07-29 23:33:19 +0900

    ui(TitleHead): adaptive poster and info layout

--- lib/features/title_info/components/title_head.dart
+++ lib/features/title_info/components/title_head.dart
@@ -11,76 +11,113 @@ class TitleHead extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ResponsiveValue<Widget>(
+      context,
+      defaultValue: _buildSmallLayout(context),
+      valueWhen: [
+        Condition.largerThan(
+          name: MOBILE,
+          value: _buildLargeLayout(context),
+        )
+      ],
+    ).value!;
+  }
+
+  Widget _buildSmallLayout(BuildContext context) {
+    return Column(
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 320),
+            child: _buildPoster(),
+          ),
+        ),
+        BlankSpace.bottom(16),
+        _buildInfo(context, true),
+      ],
+    );
+  }
+
+  Widget _buildLargeLayout(BuildContext context) {
     return Row(
       children: [
         SizedBox(
           height: ResponsiveValue(
             context,
-            defaultValue: 550 / 3.75,
+            defaultValue: 550 / 3.5,
             valueWhen: const [
-              Condition.largerThan(name: MOBILE, value: 550 / 3.5),
-              Condition.largerThan(name: TABLET, value: 550 / 3),
+              Condition.largerThan(name: MOBILE, value: 550 / 3),
+              Condition.largerThan(name: TABLET, value: 550 / 2.5),
             ],
           ).value,
-          child: AspectRatio(
-            aspectRatio: 7.0 / 10.0,
-            child: Hero(
-              tag: model.id!,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Stack(
-                  children: [
-                    Container(color: Colors.black12),
-                    FadeInImage.memoryNetwork(
-                      image: kStaticUrl.toString() +
-                          (model.posters?.original?.url ?? ''),
-                      placeholder: kTransparentImage,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          child: _buildPoster(),
         ),
         BlankSpace.right(16),
         Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: _buildInfo(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPoster() {
+    return AspectRatio(
+      aspectRatio: 7.0 / 10.0,
+      child: Hero(
+        tag: model.id!,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Stack(
             children: [
-              Text(
-                model.names!.ru ??
-                    model.names!.alternative ??
-                    model.names!.en ??
-                    '[Без навзвания]',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6!
-                    .copyWith(fontSize: 20),
+              Container(color: Colors.black12),
+              FadeInImage.memoryNetwork(
+                image: kStaticUrl.toString() +
+                    (model.posters?.original?.url ?? ''),
+                placeholder: kTransparentImage,
+                fit: BoxFit.cover,
               ),
-              BlankSpace.bottom(2),
-              if (model.names!.en != null)
-                Text(
-                  model.names!.en!,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5!
-                      .copyWith(fontSize: 15),
-                ),
-              BlankSpace.bottom(2),
-              ActionChip(
-                avatar: const Icon(Icons.star_border, size: 18),
-                label: Text(
-                  (model.inFavorites ?? 0).toString(),
-                  style: const TextStyle(fontSize: 14),
-                ),
-                backgroundColor: Colors.transparent,
-                onPressed: () {},
-                shape: const StadiumBorder(side: BorderSide(width: 0.2)),
-              )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildInfo(BuildContext context, [bool center = false]) {
+    final stars = ActionChip(
+      avatar: const Icon(Icons.star_border, size: 18),
+      label: Text(
+        (model.inFavorites ?? 0).toString(),
+        style: const TextStyle(fontSize: 14),
+      ),
+      backgroundColor: Colors.transparent,
+      onPressed: () {},
+      shape: const StadiumBorder(side: BorderSide(width: 0.2)),
+    );
+
+    return Column(
+      crossAxisAlignment:
+          center ? CrossAxisAlignment.center : CrossAxisAlignment.start,
+      children: [
+        Text(
+          model.names!.ru ??
+              model.names!.alternative ??
+              model.names!.en ??
+              '[Без навзвания]',
+          style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: 20),
+          textAlign: center ? TextAlign.center : null,
+        ),
+        BlankSpace.bottom(4),
+        if (model.names!.en != null)
+          Text(
+            model.names!.en!,
+            style:
+                Theme.of(context).textTheme.headline5!.copyWith(fontSize: 15),
+            textAlign: center ? TextAlign.center : null,
+          ),
+        BlankSpace.bottom(4),
+        center ? Center(child: stars) : stars,
       ],
     );
   }
